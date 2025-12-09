30e38de05c1ec33d4c41330404fa45de25ff30b6
Bharathreddy-1010 <bharath404074@gmail.com>
2025-12-09 09:45:33 +0530

    Update backend for stable deployment

--- backend/main.py
+++ backend/main.py
@@ -16,10 +16,9 @@ from PIL import Image
 app = FastAPI()
 
 # --- CORS SETUP ---
-origins = ["http://localhost:3000"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

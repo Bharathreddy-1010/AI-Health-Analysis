ff7250bdc075151cb289b38138e5c2c75cfd45d8
Bharathreddy-1010 <bharath404074@gmail.com>
2026-01-17 15:49:13 +0530

    Hardcoding base url to test

--- ai-diet-planner/src/AuthPages.js
+++ ai-diet-planner/src/AuthPages.js
@@ -76,7 +76,7 @@ export const LoginPage = ({ onLogin, onSwitch }) => {
 
     try {
       // Connecting to Python Backend
-      const response = await fetch(`${apiBaseUrl}/login`, {
+      const response = await fetch(`https://ai-health-analysis-1.onrender.com/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ 
@@ -155,7 +155,7 @@ export const SignupPage = ({ onSwitch }) => {
     setLoading(true);
 
     try {
-      const response = await fetch(`${apiBaseUrl}/signup`, {
+      const response = await fetch(`https://ai-health-analysis-1.onrender.com/signup`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ 
